c37399afe0391e6bb1fc38d30b944ac2baab7089
UltraCat567 <sonihkabobop@gmail.com>
2025-01-31 21:33:42 +0300

    (

--- Basketball/Assets/script/TriggerBall.cs
+++ Basketball/Assets/script/TriggerBall.cs
@@ -4,18 +4,20 @@ using TMPro;
 using UnityEngine;
 
 public class TriggerBall : MonoBehaviour
-
 {
+
     [SerializeField] int _numderBall;
     [SerializeField] TextMeshProUGUI _textMeshPro;
     [SerializeField] GameObject _textNO;
     [SerializeField] GameObject _textYES;
     [SerializeField] GameObject _textNO3;
     [SerializeField] GameObject _textYES3;
+    [SerializeField] GameObject _ice;
     private void OnTriggerEnter(Collider other)
     {
         _numderBall += 1;
         _textMeshPro.text = _numderBall.ToString();
+        Instantiate(_ice, transform.position, transform.rotation);
         if (_numderBall == 1)
         {
             _textNO.SetActive(false);
